83303b17021ba430bf18228293098fd357dd49f6
liz <lizhanzhan@jimeng.io>
2019-11-20 15:25:28 +0800

    切片

--- slice/main.go
+++ slice/main.go
@@ -24,5 +24,18 @@ func main() {
 
 	s1 := []int{0, 1, 2, 3, 8: 100}
 	fmt.Println(s1, len(s1), cap(s1))
+	s2 := []int{1, 2}
+	s2 = append(s2, 4, 5, 6)
+	fmt.Printf("len=%d, cap=%d", len(s2), cap(s2))
+	fmt.Println(s2)
 
+	s3 := []int{1, 2}
+	s3 = append(s3, 4)
+	s3 = append(s3, 5)
+	s3 = append(s3, 6)
+
+	fmt.Printf("len=%d, cap=%d", len(s3), cap(s3))
+	fmt.Println(s3)
+
+	fmt.Println(2 / 4)
 }
