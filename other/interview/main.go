94082b8931f9a100ed2ce931c764d73b9ac30be4
lizhanzhan <rick.lizhan@gmail.com>
2020-09-03 08:19:00 +0800

    interview

--- other/interview/main.go
+++ other/interview/main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func main() {
-	ch := make(chan struct{})
+//func main() {
+//	ch := make(chan struct{})
+//	go func() {
+//		fmt.Println("start working")
+//		time.Sleep(time.Second * 1)
+//		ch <- struct{}{}
+//	}()
+//
+//	<-ch
+//
+//	fmt.Println("finished")
+//}
+
+func gen(ctx context.Context) <-chan int {
+	ch := make(chan int)
 	go func() {
-		fmt.Println("start working")
-		time.Sleep(time.Second * 1)
-		ch <- struct{}{}
+		var n int
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- n:
+				n++
+				time.Sleep(time.Second)
+			}
+		}
 	}()
+	return ch
+}
 
-	<-ch
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // 避免其他地方忘记 cancel，且重复调用不影响
 
-	fmt.Println("finished")
+	for n := range gen(ctx) {
+		fmt.Println(n)
+		if n == 5 {
+			cancel()
+			break
+		}
+	}
+	// ……
 }
