cea34b2988f84247075b649aa4b4363e1d46b0c7
lizhanzhan <rick.lizhan@gmail.com>
2020-09-04 18:30:27 +0800

    goroutine

--- other/interview/main.go
+++ other/interview/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"runtime"
-	"sync"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -39,26 +38,11 @@ func gen(ctx context.Context) <-chan int {
 }
 
 func main() {
-	jobsCount := 100
-	group := sync.WaitGroup{}
-	var jobsChan = make(chan int, 3)
-	// a) 生成指定数目的 goroutine，每个 goroutine 消费 jobsChan 中的数据
-	poolCount := 3
-	for i := 1; i < poolCount; i++ {
-		go func() {
-			for j := range jobsChan {
-				fmt.Printf("hello %d\n", j)
-				time.Sleep(time.Second)
-				group.Done()
-			}
-		}()
-	}
-	// b) 把 job 依次推送到 jobsChan 供 goroutine 消费
-	for i := 0; i < jobsCount; i++ {
-		jobsChan <- i
-		group.Add(1)
-		fmt.Printf("index: %d,goroutine Num: %d\n", i, runtime.NumGoroutine())
-	}
-	group.Wait()
-	fmt.Println("done!")
+	mux := http.NewServeMux()
+
+	rh := http.RedirectHandler("http://www.baidu.com", 307)
+	mux.Handle("/foo", rh)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", mux)
 }
