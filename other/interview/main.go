1d597410ab81e33bce91f1c486b8bb64f5507963
lizhanzhan <rick.lizhan@gmail.com>
2020-09-16 19:14:55 +0800

    interview

--- other/interview/main.go
+++ other/interview/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"context"
-	"log"
-	"math/rand"
-	"strconv"
-	"sync"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
 	"time"
 )
 
@@ -40,105 +39,112 @@ func gen(ctx context.Context) <-chan int {
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	log.SetFlags(0)
 
-	// ...
-	const MaxRandomNumber = 100000
-	const NumReceivers = 10
-	const NumSenders = 1000
-
-	wgReceivers := sync.WaitGroup{}
-	wgReceivers.Add(NumReceivers)
-
-	// ...
-	dataCh := make(chan int, 100)
-	stopCh := make(chan struct{})
-	// stopCh is an additional signal channel.
-	// Its sender is the moderator goroutine shown below.
-	// Its reveivers are all senders and receivers of dataCh.
-	toStop := make(chan string, 1)
-	// the channel toStop is used to notify the moderator
-	// to close the additional signal channel (stopCh).
-	// Its senders are any senders and receivers of dataCh.
-	// Its reveiver is the moderator goroutine shown below.
-
-	var stoppedBy string
-
-	// moderator
-	go func() {
-		stoppedBy = <-toStop // part of the trick used to notify the moderator
-		// to close the additional signal channel.
-		close(stopCh)
-	}()
-
-	// senders
-	for i := 0; i < NumSenders; i++ {
-		go func(id string) {
-			for {
-				value := rand.Intn(MaxRandomNumber)
-				if value == 0 {
-					// here, a trick is used to notify the moderator
-					// to close the additional signal channel.
-					select {
-					case toStop <- "sender#" + id:
-					default:
-					}
-					return
-				}
-
-				// the first select here is to try to exit the
-				// goroutine as early as possible.
-				select {
-				case <-stopCh:
-					return
-				default:
-				}
-
-				select {
-				case <-stopCh:
-					return
-				case dataCh <- value:
-				}
-			}
-		}(strconv.Itoa(i))
+	// 开启pprof，监听请求
+	ip := "0.0.0.0:5555"
+	if err := http.ListenAndServe(ip, nil); err != nil {
+		fmt.Printf("start pprof failed on %s\n", ip)
 	}
 
-	// receivers
-	for i := 0; i < NumReceivers; i++ {
-		go func(id string) {
-			defer wgReceivers.Done()
-
-			for {
-				// same as senders, the first select here is to
-				// try to exit the goroutine as early as possible.
-				select {
-				case <-stopCh:
-					return
-				default:
-				}
-
-				select {
-				case <-stopCh:
-					return
-				case value := <-dataCh:
-					if value == MaxRandomNumber-1 {
-						// the same trick is used to notify the moderator
-						// to close the additional signal channel.
-						select {
-						case toStop <- "receiver#" + id:
-						default:
-						}
-						return
-					}
-
-					log.Println(value)
-				}
-			}
-		}(strconv.Itoa(i))
-	}
-
-	// ...
-	wgReceivers.Wait()
-	log.Println("stopped by", stoppedBy)
+	//rand.Seed(time.Now().UnixNano())
+	//log.SetFlags(0)
+	//
+	//// ...
+	//const MaxRandomNumber = 100000
+	//const NumReceivers = 10
+	//const NumSenders = 1000
+	//
+	//wgReceivers := sync.WaitGroup{}
+	//wgReceivers.Add(NumReceivers)
+	//
+	//// ...
+	//dataCh := make(chan int, 100)
+	//stopCh := make(chan struct{})
+	//// stopCh is an additional signal channel.
+	//// Its sender is the moderator goroutine shown below.
+	//// Its reveivers are all senders and receivers of dataCh.
+	//toStop := make(chan string, 1)
+	//// the channel toStop is used to notify the moderator
+	//// to close the additional signal channel (stopCh).
+	//// Its senders are any senders and receivers of dataCh.
+	//// Its reveiver is the moderator goroutine shown below.
+	//
+	//var stoppedBy string
+	//
+	//// moderator
+	//go func() {
+	//	stoppedBy = <-toStop // part of the trick used to notify the moderator
+	//	// to close the additional signal channel.
+	//	close(stopCh)
+	//}()
+	//
+	//// senders
+	//for i := 0; i < NumSenders; i++ {
+	//	go func(id string) {
+	//		for {
+	//			value := rand.Intn(MaxRandomNumber)
+	//			if value == 0 {
+	//				// here, a trick is used to notify the moderator
+	//				// to close the additional signal channel.
+	//				select {
+	//				case toStop <- "sender#" + id:
+	//				default:
+	//				}
+	//				return
+	//			}
+	//
+	//			// the first select here is to try to exit the
+	//			// goroutine as early as possible.
+	//			select {
+	//			case <-stopCh:
+	//				return
+	//			default:
+	//			}
+	//
+	//			select {
+	//			case <-stopCh:
+	//				return
+	//			case dataCh <- value:
+	//			}
+	//		}
+	//	}(strconv.Itoa(i))
+	//}
+	//
+	//// receivers
+	//for i := 0; i < NumReceivers; i++ {
+	//	go func(id string) {
+	//		defer wgReceivers.Done()
+	//
+	//		for {
+	//			// same as senders, the first select here is to
+	//			// try to exit the goroutine as early as possible.
+	//			select {
+	//			case <-stopCh:
+	//				return
+	//			default:
+	//			}
+	//
+	//			select {
+	//			case <-stopCh:
+	//				return
+	//			case value := <-dataCh:
+	//				if value == MaxRandomNumber-1 {
+	//					// the same trick is used to notify the moderator
+	//					// to close the additional signal channel.
+	//					select {
+	//					case toStop <- "receiver#" + id:
+	//					default:
+	//					}
+	//					return
+	//				}
+	//
+	//				log.Println(value)
+	//			}
+	//		}
+	//	}(strconv.Itoa(i))
+	//}
+	//
+	//// ...
+	//wgReceivers.Wait()
+	//log.Println("stopped by", stoppedBy)
 }
