ec05513aa4c12d634222c2a2346d6eca08bbfbdf
rick.lizhan@gmail.com <rick.lizhan@gmail.com>
2021-01-03 17:33:03 +0800

    go中goroutine导致的内存泄露的场景

--- goroutine/main.go
+++ goroutine/main.go
@@ -4,14 +4,49 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	// 开启pprof，监听请求
 	ip := "127.0.0.1:6069"
-	if err := http.ListenAndServe(ip, nil); err != nil {
-		fmt.Printf("start pprof failed on %s\n", ip)
+	// 开启pprof
+	go func() {
+		if err := http.ListenAndServe(ip, nil); err != nil {
+			fmt.Printf("start pprof failed on %s\n", ip)
+			os.Exit(1)
+		}
+	}()
+
+	// 路由，访问，触发内存泄露的代码判断
+	http.HandleFunc("/test", handler)
+
+	// 阻塞
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-c
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			time.Sleep(time.Second)
+			return
+		case syscall.SIGHUP:
+		default:
+			return
+		}
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// goroutine泄露判断
+	ch := make(chan int, 1)
+	go func() {
+		<-ch
+		fmt.Println(111)
+	}()
 }
 
 //func query() int {
