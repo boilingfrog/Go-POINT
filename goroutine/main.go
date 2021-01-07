509141283f5d010927365bcd27988343a5db2bdc
rick.lizhan@gmail.com <rick.lizhan@gmail.com>
2021-01-07 09:23:30 +0800

    string源码刨铣

--- goroutine/main.go
+++ goroutine/main.go
@@ -4,41 +4,44 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
+var base string
+
 func main() {
-	// 开启pprof，监听请求
-	ip := "127.0.0.1:6069"
-	// 开启pprof
-	go func() {
-		if err := http.ListenAndServe(ip, nil); err != nil {
-			fmt.Printf("start pprof failed on %s\n", ip)
-			os.Exit(1)
-		}
-	}()
 
-	// 路由，访问，触发内存泄露的代码判断
-	http.HandleFunc("/test", handler)
-
-	// 阻塞
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
-	for {
-		s := <-c
-		switch s {
-		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			time.Sleep(time.Second)
-			return
-		case syscall.SIGHUP:
-		default:
-			return
-		}
-	}
+	fmt.Println(12)
 }
+
+//func main() {
+//	// 开启pprof，监听请求
+//	ip := "127.0.0.1:6069"
+//	// 开启pprof
+//	go func() {
+//		if err := http.ListenAndServe(ip, nil); err != nil {
+//			fmt.Printf("start pprof failed on %s\n", ip)
+//			os.Exit(1)
+//		}
+//	}()
+//
+//	// 路由，访问，触发内存泄露的代码判断
+//	http.HandleFunc("/test", handler)
+//
+//	// 阻塞
+//	c := make(chan os.Signal, 1)
+//	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+//	for {
+//		s := <-c
+//		switch s {
+//		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+//			time.Sleep(time.Second)
+//			return
+//		case syscall.SIGHUP:
+//		default:
+//			return
+//		}
+//	}
+//}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// 接收端收到的channel为nil
--- string/README.md
+++ string/README.md
@@ -1,5 +1,34 @@
-## string操作的几种方法
+## go中string是如何实现的呢
 
 
-### strings
+### 前言
 
+go中的string可谓是用到的最频繁的关键词之一了，如何实现，我们来探究下  
+
+### 实现
+
+string我们看起来是一个整体，但是本质上是一片连续的内存空间，我们也可以将它理解成一个由字符组成的数组，相比于切片仅仅少了一个Cap属性。  
+
+`src/reflect/value.go`  
+```go
+type StringHeader struct {
+	Data uintptr
+	Len  int
+}
+```
+
+切片的数据结构
+
+```go
+type SliceHeader struct {
+	Data uintptr
+	Len  int
+	Cap  int
+}
+```
+
+1、相比于切片少了一个容量的cap字段，就意味着string是不能发生地址空间扩容；  
+
+2、可以把string当成一个只读的切片类型；  
+
+3、string本身的切片是只读的，所以不会直接向字符串直接追加元素改变其本身的内存空间，所有在字符串上的写入操作都是通过拷贝实现的。  
