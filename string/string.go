ad53bc272fd48a7c6ea398f5be5a2a2903f5701a
rick.lizhan@gmail.com <rick.lizhan@gmail.com>
2021-01-09 23:31:22 +0800

    内存泄露

--- string/string.go
+++ string/string.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sync"
 )
 
 func main() {
-	strs := []string{"小白", "小红", "小李"}
-	res := strings.Join(strs, ",")
-	fmt.Println(res)
+	var (
+		jobsChan = make(chan int, 10)
+		rw       sync.RWMutex
+		num      int
+	)
+
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer wg.Done()
+			for redPacket := range jobsChan {
+				_ = redPacket
+				rw.Lock()
+				num++
+				rw.Unlock()
+			}
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		jobsChan <- i + 1
+	}
+	close(jobsChan)
+	wg.Wait()
+	fmt.Println(num)
 }
