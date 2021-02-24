4cc6ad1a80139a095bf4490f18b3e954e530d4dd
liz <rick.lizhan@gmail.com>
2021-02-24 09:28:56 +0800

    内存对齐

--- gc/main.go
+++ gc/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"unsafe"
+)
+
+type s struct {
+	Bool   bool
+	Int64  int64
+	String string
+}
 
 func main() {
+	fmt.Printf("bool size: %d\n", unsafe.Sizeof(bool(true)))
+	fmt.Printf("int32 size: %d\n", unsafe.Sizeof(int32(0)))
+	fmt.Printf("int8 size: %d\n", unsafe.Sizeof(int8(0)))
+	fmt.Printf("int64 size: %d\n", unsafe.Sizeof(int64(0)))
+	fmt.Printf("byte size: %d\n", unsafe.Sizeof(byte(0)))
+	fmt.Printf("string size: %d\n", unsafe.Sizeof("E"))
 
+	part1 := s{}
+
+	fmt.Printf("part1 size: %d, align: %d\n", unsafe.Sizeof(part1), unsafe.Alignof(part1))
+}
+
+func test1() {
 	wg := sync.WaitGroup{}
 	wg.Add(10)
 	for i := 0; i < 10; i++ {
@@ -17,5 +39,4 @@ func main() {
 	}
 
 	wg.Wait()
-
 }
