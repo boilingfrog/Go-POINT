afabc5cbb055d2e67845c9cf4ce45db2af3b16c3
liz <rick.lizhan@gmail.com>
2021-02-26 08:54:18 +0800

    内存对齐

--- gc/main.go
+++ gc/main.go
@@ -7,35 +7,38 @@ import (
 )
 
 type s struct {
-	Bool bool
-	//Int64 int64
+	Bool  bool
+	Byte  byte
+	In32  int32
+	Int64 int64
+	Int8  int8 //Int64 int64
 	//
 	//Byte byte
 	//In32 int32
 	//Int8 int8
 }
 
-//func main() {
-//	fmt.Printf("bool size: %d\n", unsafe.Sizeof(bool(true)))
-//	fmt.Printf("int32 size: %d\n", unsafe.Sizeof(int32(0)))
-//	fmt.Printf("int8 size: %d\n", unsafe.Sizeof(int8(0)))
-//	fmt.Printf("int64 size: %d\n", unsafe.Sizeof(int64(0)))
-//	fmt.Printf("byte size: %d\n", unsafe.Sizeof(byte(0)))
-//	fmt.Printf("string size: %d\n", unsafe.Sizeof("E"))
-//
-//	part1 := s{}
-//	fmt.Printf("part1 size: %d, align: %d\n", unsafe.Sizeof(part1), unsafe.Alignof(part1))
-//}
-
 func main() {
-	fmt.Printf("bool align: %d\n", unsafe.Alignof(bool(true)))
-	fmt.Printf("int32 align: %d\n", unsafe.Alignof(int32(0)))
-	fmt.Printf("int8 align: %d\n", unsafe.Alignof(int8(0)))
-	fmt.Printf("int64 align: %d\n", unsafe.Alignof(int64(0)))
-	fmt.Printf("byte align: %d\n", unsafe.Alignof(byte(0)))
-	fmt.Printf("string align: %d\n", unsafe.Alignof("EDDYCJY"))
-	fmt.Printf("map align: %d\n", unsafe.Alignof(map[string]string{}))
+	fmt.Printf("bool size: %d\n", unsafe.Sizeof(bool(true)))
+	fmt.Printf("int32 size: %d\n", unsafe.Sizeof(int32(0)))
+	fmt.Printf("int8 size: %d\n", unsafe.Sizeof(int8(0)))
+	fmt.Printf("int64 size: %d\n", unsafe.Sizeof(int64(0)))
+	fmt.Printf("byte size: %d\n", unsafe.Sizeof(byte(0)))
+	fmt.Printf("string size: %d\n", unsafe.Sizeof("E"))
+
+	part1 := s{}
+	fmt.Printf("part1 size: %d, align: %d\n", unsafe.Sizeof(part1), unsafe.Alignof(part1))
 }
+
+//func main() {
+//	fmt.Printf("bool align: %d\n", unsafe.Alignof(bool(true)))
+//	fmt.Printf("int32 align: %d\n", unsafe.Alignof(int32(0)))
+//	fmt.Printf("int8 align: %d\n", unsafe.Alignof(int8(0)))
+//	fmt.Printf("int64 align: %d\n", unsafe.Alignof(int64(0)))
+//	fmt.Printf("byte align: %d\n", unsafe.Alignof(byte(0)))
+//	fmt.Printf("string align: %d\n", unsafe.Alignof("EDDYCJY"))
+//	fmt.Printf("map align: %d\n", unsafe.Alignof(map[string]string{}))
+//}
 
 func test1() {
 	wg := sync.WaitGroup{}
