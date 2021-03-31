f996bd201f1829efbea133c96a584fed5dd37616
liz <rick.lizhan@gmail.com>
2021-03-31 14:38:04 +0800

    semaphore

--- sync/semaphore/main.go
+++ sync/semaphore/main.go
@@ -3,57 +3,78 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync"
-	"time"
+	"log"
+	"runtime"
 
 	"golang.org/x/sync/semaphore"
 )
 
-func doSomething(u string) { // 模拟抓取任务的执行
-	fmt.Println(u)
-	time.Sleep(2 * time.Second)
+// Example_workerPool demonstrates how to use a semaphore to limit the number of
+// goroutines working on parallel tasks.
+//
+// This use of a semaphore mimics a typical “worker pool” pattern, but without
+// the need to explicitly shut down idle workers when the work is done.
+func main() {
+	ctx := context.Background()
+
+	var (
+		maxWorkers = runtime.GOMAXPROCS(0)
+		sem        = semaphore.NewWeighted(int64(maxWorkers))
+		out        = make([]int, 32)
+	)
+
+	// Compute the output using up to maxWorkers goroutines at a time.
+	for i := range out {
+		// When maxWorkers goroutines are in flight, Acquire blocks until one of the
+		// workers finishes.
+		if err := sem.Acquire(ctx, 1); err != nil {
+			log.Printf("Failed to acquire semaphore: %v", err)
+			break
+		}
+
+		go func(i int) {
+			defer sem.Release(1)
+			out[i] = collatzSteps(i + 1)
+		}(i)
+	}
+
+	// Acquire all of the tokens to wait for any remaining workers to finish.
+	//
+	// If you are already waiting for the workers by some other means (such as an
+	// errgroup.Group), you can omit this final Acquire call.
+	if err := sem.Acquire(ctx, int64(maxWorkers)); err != nil {
+		log.Printf("Failed to acquire semaphore: %v", err)
+	}
+
+	fmt.Println(out)
+
+	// Output:
+	// [0 1 7 2 5 8 16 3 19 6 14 9 9 17 17 4 12 20 20 7 7 15 15 10 23 10 111 18 18 18 106 5]
 }
 
-const (
-	Limit  = 3 // 同時并行运行的goroutine上限
-	Weight = 1 // 每个goroutine获取信号量资源的权重
-)
-
-func main() {
-	urls := []string{
-		"http://www.example.com",
-		"http://www.example.net",
-		"http://www.example.net/foo",
-		"http://www.example.net/bar",
-		"http://www.example.net/baz",
-		"http://www.example.com",
-		"http://www.example.net",
-		"http://www.example.net/foo",
-		"http://www.example.net/bar",
-		"http://www.example.net/baz",
-		"http://www.example.com",
-		"http://www.example.net",
-		"http://www.example.net/foo",
-		"http://www.example.net/bar",
-		"http://www.example.net/baz",
-		"http://www.example.com",
-		"http://www.example.net",
-		"http://www.example.net/foo",
-		"http://www.example.net/bar",
-		"http://www.example.net/baz",
+// collatzSteps computes the number of steps to reach 1 under the Collatz
+// conjecture. (See https://en.wikipedia.org/wiki/Collatz_conjecture.)
+func collatzSteps(n int) (steps int) {
+	if n <= 0 {
+		panic("nonpositive input")
 	}
-	s := semaphore.NewWeighted(Limit)
-	var w sync.WaitGroup
-	for _, u := range urls {
-		w.Add(1)
-		go func(u string) {
-			s.Acquire(context.Background(), Weight)
-			doSomething(u)
-			s.Release(Weight)
-			w.Done()
-		}(u)
-	}
-	w.Wait()
 
-	fmt.Println("All Done")
+	for ; n > 1; steps++ {
+		if steps < 0 {
+			panic("too many steps")
+		}
+
+		if n%2 == 0 {
+			n /= 2
+			continue
+		}
+
+		const maxInt = int(^uint(0) >> 1)
+		if n > (maxInt-1)/3 {
+			panic("overflow")
+		}
+		n = 3*n + 1
+	}
+
+	return steps
 }
