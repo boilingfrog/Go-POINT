8e60c93eed9888f3d93fe2fdbeb1b47193c54ef5
liz <rick.lizhan@gmail.com>
2025-06-17 00:27:15 +0800

    feat: update

--- golang/go_mistakes/main.go
+++ golang/go_mistakes/main.go
@@ -11,8 +11,11 @@ func main() {
 	slice2 := []int{5, 4, 3}
 	fmt.Printf("%p\n", slice1)
 	copy(slice1, slice2)
+
 	slice2[0] = 9
+
 	slice1[3] = 9
+
 	fmt.Printf("%p\n", slice1)
 
 	//id := "631efd32a2deab22008da983"
