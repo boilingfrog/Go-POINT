1b6f2185ac3a438993e5617b0bfe8b68f7343b17
liz <rick.lizhan@gmail.com>
2025-07-06 22:59:25 +0800

    feat: cursor 使用

--- git/main.go
+++ git/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
-	fmt.Println(CompareNumbers(10, 100))
+	fmt.Println(GetMaxNumber(10, 100))
+}
+
+func GetMaxNumber(a, b int) int {
+	log.Printf("Comparing numbers: a=%d, b=%d", a, b)
+
+	if a > b {
+		log.Printf("Result: %d > %d", a, b)
+		return a
+	} else if a < b {
+		log.Printf("Result: %d < %d", a, b)
+		return b
+	} else {
+		log.Printf("Result: %d == %d", a, b)
+		return a
+	}
 }
